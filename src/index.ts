fadf6a2b2e362fe79bc1a7341cc179a91f58f7a9
Banks Nussman <banks@nussman.us>
2025-02-12 22:15:42 -0500

    export the raw data

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ type Make = keyof typeof data;
 /**
  * getModels
  * Returns all known models from a specific make of car.
- * This will return an empty array if the make is not known by this package. 
+ * This will return an empty array if the make is not known by this package.
  * @param make the name of a make of a car (for example Ford, Toyota, Honda)
  * @returns list of models
  */
@@ -16,9 +16,11 @@ export function getModels(make: string) {
 }
 
 /**
- * getMakes 
- * @returns an array of all of the known makes (for example Ford, Toyota, Honda) 
+ * getMakes
+ * @returns an array of all of the known makes (for example Ford, Toyota, Honda)
  */
 export function getMakes() {
   return Object.keys(data) as Make[];
 }
+
+export { data };
